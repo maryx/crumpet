0fbdcac77db24e789ab75266d553841494d7a609
Mary Xia <mxia@yahoo-inc.com>
2014-12-05 22:22:00 -0800

    \cite works on the entire document body

---
+++ js/cite.js
@@ -0,0 +1,26 @@
+/**
+   Parses \cite{#reference} into <a href='#reference>(Author, Year)</a>
+*/
+
+var references = {
+    book1: {
+        authorLastName: 'Knuth',
+        authorFirstName: 'Donald',
+        title: 'Concrete Mathematics',
+        year: 1994,
+        publisher: 'Addison-Wesley',
+        address: 'Reading'
+    }
+};
+
+function parseLinks() {
+    var linkRegex = /(\\cite{#)([^}]*)(})/g,
+        hrefEnd = '</a>';
+    $('body').html(
+        $('body').html().replace(linkRegex, function($0, $1, $2) {
+            var href = '<a href="#' + $2 + '">',
+            citation = '(' + references[$2].authorLastName + ', ' + references[$2].year + ')';
+            return href + citation + hrefEnd;
+        })
+    );    
+}
